dc7348a59e61f1d44c5ccffcd8a4158f44408c01
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-12-19 20:21:51 +0900

    Add some configs for permalink

--- config.rb
+++ config.rb
@@ -6,9 +6,9 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
@@ -64,6 +64,7 @@ page "/feed.xml", layout: false
 ###
 # Helpers
 ###
+activate :directory_indexes
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
