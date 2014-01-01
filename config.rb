61d8677248118b8220d79dfbe967ce6ce60fd288
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-01-01 22:00:20 +0900

    Add :deploy settings

--- config.rb
+++ config.rb
@@ -104,3 +104,11 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  # deploy.branch   = "custom-branch" # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+end
