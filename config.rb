2251527c22f06ad59a34291c0b23bb011df49aff
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-01-01 23:54:59 +0900

    Add new_article_template

--- config.rb
+++ config.rb
@@ -27,6 +27,7 @@ activate :blog do |blog|
   blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
+  blog.new_article_template = 'new_article_template.haml'
 end
 
 page "/feed.xml", layout: false
